576d8657dfb1b7223e36647d5d6aa9f606463e1a
Sharebear171 <sharymasood1@gmail.com>
2021-03-11 00:27:13 -0500

    Added JDA support (edited pom.xml and Main.java)

--- ChefWumpus/ChefWumpusBot/src/main/java/Main.java
+++ ChefWumpus/ChefWumpusBot/src/main/java/Main.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
 
 public class Main {
+
+    public static void main(String [] args){
+        JDABuilder
+    }
 }
