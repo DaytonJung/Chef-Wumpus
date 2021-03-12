9fff8c271230cfd7f3aa3ae1eec2b9d1f3fb19f9
Sharebear171 <sharymasood1@gmail.com>
2021-03-12 17:54:55 -0500

    First Bot Code

--- ChefWumpus/ChefWumpusBot/src/main/java/Main.java
+++ ChefWumpus/ChefWumpusBot/src/main/java/Main.java
@@ -1,8 +1,18 @@
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
+import javax.security.auth.login.LoginException;
+
 public class Main {
 
     public static void main(String [] args){
+        JDABuilder builder = JDABuilder.createDefault("ODE0MjY1Nzg5MTAzNzM0ODE0.YDbWHA.DuUpNkBCt3HENZNxd7bDLGIBTk4");
+        JDA jda = null;
+        try {
+            jda = builder.build();
+        }
+        catch(LoginException e){
+            e.printStackTrace();
+        }
     }
 }
